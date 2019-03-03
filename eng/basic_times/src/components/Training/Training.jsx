2e23c7d81cc822b28cb3ddd941a8a9c134082f1c
Alexander Smirnov <a.smirnov@space307.com>
2019-03-03 20:13:11 +0300

    basic start training

---
+++ eng/basic_times/src/components/Training/Training.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+const question = [
+  {
+    pos:1,
+    verb:'go',
+    q:'Ты пойдёшь?',
+  },
+  {
+    pos:1,
+    verb:'go',
+    q:'Он идёт.',
+  }
+];
+
+
+class Training extends React.Component {
+  render() {
+    return <h1>232323</h1>;
+  }
+}
+
+export default Training;
--- eng/basic_times/src/components/index.jsx
+++ eng/basic_times/src/components/index.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import Table from './Table/table.jsx';
+import Training from './Training/Training.jsx';
 const love = ['love', 'loves', 'loved'];
 const go = ['go', 'goes', 'went'];
 class Entry extends React.Component {
   render() {
     return <div>
       <Table verb={go}/>
+      <Training/>
     </div>;
   }
 }
